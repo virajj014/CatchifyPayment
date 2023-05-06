c68c3a7106c374820ba500b422d2d5cc7f0f903e
viraj <virajj014@gmail.com>
2023-05-06 22:47:52 +0530

    Added cors to payment backend of catchify

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 var port = process.env.PORT || 4000;
 
 // enable CORS
-app.use(cors({ origin: 'https://catchyfive.com/'}))
+app.use(cors({ origin: ['https://catchyfive.com/', 'http://localhost:3000']}))
 // parse application/json
 app.use(bodyParser.json());
 // parse application/x-www-form-urlencoded
